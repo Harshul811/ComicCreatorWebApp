7630d79356bd6e96cb2b7c4cd4fd04236626e500
Harshul Gupta <84350915+Harshul811@users.noreply.github.com>
2023-11-19 23:36:17 +0530

    Add files via upload

--- services/index.tsx
+++ services/index.tsx
@@ -1,1 +1,33 @@
+import axios from 'axios';
 
+const API_URL = "https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud";
+
+const query = async (payload: { inputs: string }) => {
+    try {
+        console.log(payload);
+      const response = await axios.post(API_URL, payload, {
+        headers: {
+          Accept: 'image/png',
+          Authorization: 'Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM',
+          'Content-Type': 'application/json',
+        },
+        responseType: 'arraybuffer',
+      });
+
+      const imageBase64 = Buffer.from(response.data, 'binary').toString('base64');
+      const imageSrc = `data:image/png;base64,${imageBase64}`;
+      return imageSrc;
+    } catch (error) {
+      console.error('Error querying API:', error);
+      return null;
+    }
+  };
+
+  export const handleQuery = (input: string) => {
+    const result = query({ inputs: input });
+    if (result === null){
+        return '';
+    }
+
+    return result.toString();
+  };
