cc1f27f201370aa100d030b195ab87b3b4dd89d3
Harshul Gupta <84350915+Harshul811@users.noreply.github.com>
2023-11-19 23:34:27 +0530

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,17 @@
+"use client"
+import { useState } from 'react';
+import { handleQuery } from "@/services";
 
+const Home: React.FC = () => {
+    const [imageSrc, setImageSrc] = useState<string | null>(null);
+    return (
+        <div className="p-5 sm:px-10 md:px-20">
+            
+	        <img id="comicPic" src = "https://www.thelancet.com/cms/attachment/91278e82-b394-4863-88c2-39c11fae8137/gr1.jpg" 
+            width="100px" height="100px"/>
+	        <button id="generateComic" onClick={() => {handleQuery("astronaut riding")}}>Generate Comic</button>
+        </div>
+    );
+};
+
+export default Home;
